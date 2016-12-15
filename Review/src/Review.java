ed9c10a25dc9105f590786ee983312f2b8e495a8
Manny Perez <mannyperez@Mannys-MacBook-Pro.local>
2016-12-15 16:02:34 -0600

    Modified and added Review.java class file showing variables and functions

---
+++ Review/src/Review.java
@@ -0,0 +1,37 @@
+
+public class Review {
+
+		public static void main(String[] args) {
+// 		TODO Auto-generated method stub
+//      VARIABLES
+			
+//		String myName = "Manny";
+//		float faveNumber = 5; 
+//		System.out.println(myName + "'s favorite number is " + faveNumber);
+//		Review.exampleStatic();
+		Review myReview = new Review();
+//		myReview.exampleMethod()
+//		myReview.exampleArgMethod("print this string!");
+		}
+//   	FUNCTIONS
+		
+//      Non-static
+//		public void exampleMethod(){
+//			System.out.println("exampleMethod");
+//		}
+		
+//		Static
+		
+//		public static void exampleStatic() {
+//			System.out.println("exampleStatic");
+//		}
+// 		With argument
+//		public void exampleArgMethod(String message){
+//			System.out.println(message);
+//		}
+		
+//		Arrays
+
+}
+
+
