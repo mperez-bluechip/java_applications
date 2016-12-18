6fb95d1f2581dacbe9b69b4237032b10e86ec8d9
Manny Perez <mannyperez@Mannys-MacBook-Pro.local>
2016-12-18 13:37:43 -0600

    Added NumericLiterals directory with following Numeric code programming

--- SimplifiedGenerics/src/com/lynda/javatraining/Main.java
+++ SimplifiedGenerics/src/com/lynda/javatraining/Main.java
@@ -1,5 +1,7 @@
 package com.lynda.javatraining;
 
+import java.util.ArrayList;
+
 import com.lynda.olivepress.olives.Olive;
 
 public class Main {
@@ -10,8 +12,12 @@ public class Main {
 		Olive o2 = new Olive("Picholine", 0x00FF00);
 		Olive o3 = new Olive("Ligurio", 0x000000);
 		
-		Olive[] olives = {o1, o2, o3};
-		System.out.println(olives);
+		//Olive[] olives = {o1, o2, o3};
+		ArrayList<Olive> olives2 = new ArrayList<>();
+		olives2.add(o1);
+		olives2.add(o2);
+		olives2.add(o3);
+		System.out.println(olives2);
 	}
 	
 }
