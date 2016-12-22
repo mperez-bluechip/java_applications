c3df0d043f23097f315f024f23b7353bdd7a9a8f
Manny Perez <threedyou@gmail.com>
2016-12-22 08:33:16 -0600

    Modified Main.java to throw customized exception and added customized exception file

---
+++ Throwing/src/com/lynda/javatraining/exceptions/WrongFileException.java
@@ -0,0 +1,10 @@
+package com.lynda.javatraining.exceptions;
+
+public class WrongFileException extends Exception {
+	public static final long serialVersionUID = 42L;
+	
+	@Override
+	public String getMessage() {
+		return "You chose the wrong file!";
+	}
+}
