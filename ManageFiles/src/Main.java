fb7d7cbe8b20e057569dbc199fdbb9d74bc906ce
Manny Perez <threedyou@gmail.com>
2016-12-22 15:58:10 -0600

    Modified Main.java showing copy, delete, and move methods

--- ManageFiles/src/Main.java
+++ ManageFiles/src/Main.java
@@ -15,6 +15,15 @@ public class Main {
 		Path target = Paths.get("files/newFile.txt");
 		Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
 		
+//		Path toDelete = Paths.get("Files/todelete.txt");
+//		Files.delete(toDelete);
+//		System.out.println("File deleted");
+		
+		Path newdir = Paths.get("files/newdir");
+		Files.createDirectory(newdir);
+		
+		Files.move(source, newdir.resolve(source.getFileName()),
+			StandardCopyOption.REPLACE_EXISTING);
 		
 	}
 	
