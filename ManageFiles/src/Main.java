e4e794c1b346f2498e9543c6e4003d8626993cf8
Manny Perez <threedyou@gmail.com>
2016-12-22 15:46:40 -0600

    Added new text file

--- ManageFiles/src/Main.java
+++ ManageFiles/src/Main.java
@@ -1,6 +1,9 @@
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Main {
 
@@ -8,6 +11,12 @@ public class Main {
 		
 		Path source = Paths.get("files/loremipsum.txt");
 		System.out.println(source.getFileName());
+		
+		Path target = Paths.get("files/newFile.txt");
+		Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+		
+		
 	}
-
+	
 }
+
