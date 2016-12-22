7bc3a3ace94273433ae894d5bbfc5417ecf7c4c2
Manny Perez <threedyou@gmail.com>
2016-12-22 15:33:28 -0600

    Modified Main.java to show Path class functionality

--- PathClass/src/Main.java
+++ PathClass/src/Main.java
@@ -1,6 +1,23 @@
+import java.io.IOException;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+//		Path path = Paths.get("/temp/Lorumipsum.txt");
+		
+		Path path = Paths.get("loremipsum.txt");
+		
+		System.out.println(path.toString());
+		System.out.println(path.getFileName());
+		System.out.println(path.getNameCount());
+		System.out.println(path.getName(path.getNameCount()-1));
+		
+		Path realPath = path.toRealPath(LinkOption.NOFOLLOW_LINKS);
+		System.out.println(realPath);
 		
 	}
 
