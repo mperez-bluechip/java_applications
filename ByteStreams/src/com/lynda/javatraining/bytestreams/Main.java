38b8bcaed58a26d30669a5fa23643c413dbdec64
Manny Perez <threedyou@gmail.com>
2016-12-24 12:03:01 -0600

    Added new text file using input streams

--- ByteStreams/src/com/lynda/javatraining/bytestreams/Main.java
+++ ByteStreams/src/com/lynda/javatraining/bytestreams/Main.java
@@ -1,8 +1,29 @@
 package com.lynda.javatraining.bytestreams;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		try (
+				FileInputStream in = new FileInputStream("textfile.txt");
+				FileOutputStream out = new FileOutputStream("new.txt");
+			)
+		
+		{
+		
+		int c;
+		while((c = in.read()) != -1){
+			out.write(c);
+		}  
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 	}
 }
